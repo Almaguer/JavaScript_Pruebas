7337745c13d6ab99993091f58f9aa88e84f461b2
Almaguer <juanm.almaguer@gmail.com>
2014-11-11 23:39:00 -0600

    Version3

--- Tablas/Js/tablas.js
+++ Tablas/Js/tablas.js
@@ -38,16 +38,74 @@ function escala(altura){
 escala(alturaPozo);
 */
 
-var niveles= prompt("¿Cuántos niveles quieres?");
-var contador=1;
-var proxNivel=contador+2;
-var ancho= ((2*largo)-1);
-var espacios= niveles-1;
+var niveles= Number(prompt("¿Cuántos niveles quieres?"));
+var x=1; /*se aumentará de 2 en 2 */
+var espaciosParaImprimir= niveles-1;
+var imprime="";
+var auxiliar=1;
+var numeroAsteriscos=1;
 
-function imprimeArbol(numero){
-while(contador<largo){
 
-	alert()
+function imprimeArbol(nivelesX){
+	aux2 = espaciosParaImprimir;
+	aux3 = espaciosParaImprimir;
+
+	for (var k=0; k <nivelesX ; k++) {
+
+		for (var i=1; i<= aux2; i++){
+			imprime= imprime + "-";
+			}
+		for(var j=1; j<=x; j++){
+			imprime+="*";
+			}
+		imprime= imprime + "\n";
+		aux2= aux2-1;
+		x+=2;
 	}
+alert(imprime);
+	}
+
+imprimeArbol(niveles);
+
+
+
+/*
+var rad= prompt("Dame el radio de tu circulo");
+
+var area = 0;
+
+function areaCirculo(radio){
+	area= ((radio*radio)*3.1416);
+	alert(area);
 }
 
+areaCirculo(rad);
+
+function areaCirculoSin(){
+	var rad=prompt("dame el radio deseado");
+	area= ((rad*rad)*3.1416);
+	alert(area);
+}
+
+areaCirculoSin();
+
+var ladoPent= prompt("Dame el largo de un lado del pentágono");
+var apotemaUser= prompt("dame el largo del apotema");
+
+function areaPentagono(lado,apotema){
+	var perimetroPentagono= Number(lado*5);
+	area= Number((perimetroPentagono*apotema)/2)
+	alert(area);
+}
+areaPentagono(ladoPent,apotemaUser);
+
+function areaPentagonoSin(){
+	var ladoPentagono= prompt("Dame la longitud del lado de pentagono");
+	var perimetroPentagono= Number(ladoPentagono*5);
+	var apotema = prompt("dame el apotema del pentagono");
+	area= Number((perimetroPentagono*apotema)/2)
+	alert(area);
+}
+
+areaPentagonoSin();
+*/
